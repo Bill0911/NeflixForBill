ade914c907d04b66044020e595e4607dfb507158
FjodorSmorodins <146731564+FjodorSmorodins@users.noreply.github.com>
2024-11-28 13:51:15 +0100

    api example working

--- demo/src/main/java/com/example/demo/AuthorRepository.java
+++ demo/src/main/java/com/example/demo/AuthorRepository.java
@@ -1,6 +1,5 @@
-package com.example.demo.repository;
+package com.example.demo;
 
-import com.example.demo.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
