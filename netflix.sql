0cda38c9ffce029939a4e6664606a0f924f08324
Bill0911 <do.do@student.nhlstenden.com>
2024-12-03 15:24:35 +0100

    update DB 100%

--- netflix.sql
+++ netflix.sql
@@ -1,328 +1,348 @@
 
---
---  `netflix`
+-- Database: `netflix`
 --
 
 -- --------------------------------------------------------
 
 --
---  `language`
+-- Table structure for table `episode`
+--
+
+USE `netflix`;
+
+CREATE TABLE `episode` (
+  `episode_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `duration` time DEFAULT '00:00:00',
+  `series_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `genre_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genreaccount`
+--
+
+CREATE TABLE `genreaccount` (
+  `account_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genreformovie`
+--
+
+CREATE TABLE `genreformovie` (
+  `genre_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genreforseries`
+--
+
+CREATE TABLE `genreforseries` (
+  `genre_id` int(11) NOT NULL,
+  `series_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `language`
 --
 
 CREATE TABLE `language` (
   `language_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`language_id`)
+  `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- `language`
---
-
-INSERT INTO `language` (`language_id`, `name`) VALUES
-(1, 'eng'),
-(2, 'nl'),
-(3, 'lv');
 
 -- --------------------------------------------------------
 
 --
---  `movie`
+-- Table structure for table `movie`
 --
 
 CREATE TABLE `movie` (
-  `movie_id` int(11) PRIMARY KEY NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `duration` TIME DEFAULT '00:00:00',
-  `sd_available` bit(1),
-  `hd_available` bit(1),
-  `uhd_available` bit(1)
+  `movie_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `duration` time DEFAULT '00:00:00',
+  `sd_available` bit(1) DEFAULT NULL,
+  `hd_available` bit(1) DEFAULT NULL,
+  `uhd_available` bit(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
---  `movie`
---
-
-INSERT INTO `movie` (`movie_id`, `title`, `duration`, `age_range`) VALUES
-(1, 'dsfkghsdkgj', '01:23:00', '12'),
-(2, 'batman', '02:13:00', '12'),
-(3, 'starwars', '02:30:00', '12');
 
 -- --------------------------------------------------------
 
 --
--- `movieviewcount`
+-- Table structure for table `moviesprofilewatchlist`
+--
+
+CREATE TABLE `moviesprofilewatchlist` (
+  `profile_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movieviewcount`
 --
 
 CREATE TABLE `movieviewcount` (
   `account_id` int(11) NOT NULL,
   `movie_id` int(11) NOT NULL,
-  `number` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`account_id`, `movie_id`),
-  FOREIGN KEY (`account_id`) REFERENCES `user`(`account_id`),
-  FOREIGN KEY (`movie_id`) REFERENCES `movie`(`movie_id`)
+  `number` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- `movieviewcount`
---
-
-INSERT INTO `movieviewcount` (`account_id`, `movie_id`, `number`) VALUES
-(3, 1, 5),
-(3, 2, 6),
-(1, 3, 1);
 
 -- --------------------------------------------------------
 
 --
--- `user`
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `profile_id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `profile_image` varchar(255) DEFAULT NULL,
+  `profile_child` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `series`
+--
+
+CREATE TABLE `series` (
+  `series_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `age_range` enum('6','9','12','16','16+') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seriesprofilewatchlist`
+--
+
+CREATE TABLE `seriesprofilewatchlist` (
+  `account_id` int(11) NOT NULL,
+  `series_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seriesviewcount`
+--
+
+CREATE TABLE `seriesviewcount` (
+  `account_id` int(11) NOT NULL,
+  `series_id` int(11) NOT NULL,
+  `number` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subtitleformovie`
+--
+
+CREATE TABLE `subtitleformovie` (
+  `language_id` int(11) NOT NULL,
+  `movie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subtitleforseries`
+--
+
+CREATE TABLE `subtitleforseries` (
+  `language_id` int(11) NOT NULL,
+  `series_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `account_id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `payment_method` varchar(255) NOT NULL,
-  `blocked` bit(1) NOT NULL,
-  `subscription` enum('SD','HD','UHD') NOT NULL,
-  `trial_start_date` datetime NOT NULL,
-  `language_id` int(11) NOT NULL,
-  `role` enum('VIEWER','JUNIOR','MEDIOR','SENIOR') NOT NULL,
-  `failed_attempts` int(11) NOT NULL DEFAULT 0,
+  `payment_method` varchar(255) DEFAULT 'Credit Card',
+  `blocked` bit(1) DEFAULT b'0',
+  `subscription` enum('SD','HD','UHD') DEFAULT 'SD',
+  `trial_start_date` datetime DEFAULT current_timestamp(),
+  `language_id` int(11) DEFAULT NULL,
+  `role` enum('Viewer','Junior','Medior','Senior') DEFAULT 'Viewer',
+  `failed_attempts` int(11) DEFAULT 0,
   `lock_time` datetime DEFAULT NULL,
-  `discount` bit(1),
-  PRIMARY KEY (`account_id`),
-  FOREIGN KEY (`language_id`) REFERENCES `language`(`language_id`)
+  `discount` bit(1) DEFAULT b'0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- `user`
---
-
-INSERT INTO `user` (`account_id`, `email`, `payment_method`, `blocked`, `subscription`, `trial_start_date`, `language_id`, `role`, `password`, `failed_attempts`, `lock_time`) VALUES
-(1, 'testuser@example.com', 'Credit Card', 0, 'HD', '2024-12-01 19:59:05', 1, 'VIEWER', '$2a$10$NqV0coTNtn.xlAUNjsgoEeQpwWfsO6oY1N6vzvgR4jQsmXHz0OQvm', 0, NULL),
-(2, 'testuser@example.com', 'Credit Card', 0, 'HD', '2024-12-01 20:04:03', 1, 'VIEWER', '$2a$10$g4wVMjf7w62uYTE1wNimf.lSDVxIu/KaCkoLb/A5pmzALRrs/i8m2', 0, NULL),
-(3, 'fjodorsm@gmail.com', 'Credit Card', 0, 'HD', '2024-12-01 20:04:44', 3, 'VIEWER', '$2a$10$kCd6DisqZc3AJxmc7FMctO2BUd5Rt.6ou5oeVbApGZVwv22.BppYq', 0, NULL),
-(4, 'yarik@gmail.com', '123', 1, 'HD', '2024-12-03 00:11:11', 1, 'VIEWER', '$2a$10$2M029yo3azaltXc9wa5/feTey1M8gh8Tf3JNnBaaaNIJqtsdKKu2u', 3, NULL),
-(5, 'zhora@gmail.com', 'cash', 0, 'HD', '2024-12-03 00:12:48', 2, 'VIEWER', '$2a$10$/1PUQ1OdRl7OA3M0f.YtUuC/dI/oT/71IiShvQia1kIOGRPvj1rXu', 0, NULL);
-
---
--- Save table indexes
+-- Indexes for dumped tables
 --
 
 --
---  `language`
+-- Indexes for table `episode`
 --
-
---
--- `series profile watchlist`
---
-
-CREATE TABLE `seriesProfileWatchList` (
-  `account_id` int(11) NOT NULL,
-  `series_id` int(11) NOT NULL,
-  PRIMARY KEY (`account_id`, `series_id`),
-  FOREIGN KEY (`account_id`) REFERENCES `user`(`account_id`),
-  FOREIGN KEY (`series_id`) REFERENCES `series`(`series_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `seriesProfileWatchList` (`account_id`, `series_id`)
- VALUES (1, 2);
+ALTER TABLE `episode`
+  ADD PRIMARY KEY (`episode_id`);
 
 --
--- `movies profile watchlist`
+-- Indexes for table `genre`
 --
-
-CREATE TABLE `moviesProfileWatchList` (
-  `profile_id` int(11) NOT NULL,
-  `movie_id` int(11) NOT NULL,
-  PRIMARY KEY (`profile_id`, `movie_id`),
-  FOREIGN KEY (`profile_id`) REFERENCES `user`(`account_id`),
-  FOREIGN KEY (`movie_id`) REFERENCES `movie`(`movie_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `moviesProfileWatchList` (`profile_id`, `movie_id`) 
- VALUES (1, 2);
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`genre_id`);
 
 --
+-- Indexes for table `genreaccount`
 --
---
-
---
--- `profile`
---
-
-CREATE TABLE `profile` (
-  `profile_id` int(11) NOT NULL PRIMARY KEY,
-  `account_id` int(11) NOT NULL,
-  `profile_image` VARCHAR(255),
-  `profile_child` bit(1),
-  FOREIGN KEY (`account_id`) REFERENCES `user`(`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE `genreaccount`
+  ADD PRIMARY KEY (`account_id`,`genre_id`);
 
 --
--- genre account
+-- Indexes for table `genreformovie`
 --
-
-CREATE TABLE `genreAccount` (
-  `account_id` int(11) NOT NULL,
-  `genre_id` int(11) NOT NULL,
-  PRIMARY KEY (`account_id`, `genre_id`),
-  FOREIGN KEY (`account_id`) REFERENCES `user`(`account_id`),
-  FOREIGN KEY (`genre_id`) REFERENCES `genre`(`genre_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `genreAccount` (`account_id`, `genre_id`) 
-VALUES (1, 2);
+ALTER TABLE `genreformovie`
+  ADD PRIMARY KEY (`genre_id`,`movie_id`);
 
 --
--- genre
+-- Indexes for table `genreforseries`
 --
-
-CREATE TABLE `genre` (
-  `genre_id` int(11) NOT NULL PRIMARY KEY,
-  `name` VARCHAR(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `genre` (`genre_id`, `name`) 
-VALUES (1, 'Horror');
-
-INSERT INTO `genre` (`genre_id`, `name`)
-VALUES (2, 'Action');
+ALTER TABLE `genreforseries`
+  ADD PRIMARY KEY (`genre_id`,`series_id`);
 
 --
--- genre for movies
---
-
-CREATE TABLE `genreForMovie`(
-  `genre_id` int(11) NOT NULL,
-  `movie_id` int(11) NOT NULL,
-  PRIMARY KEY (`genre_id`, `movie_id`),
-  FOREIGN KEY (`genre_id`) REFERENCES `genre`(`genre_id`),
-  FOREIGN KEY (`movie_id`) REFERENCES `movie`(`movie_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `genreForMovie` (`genre_id`, `movie_id`) 
-VALUES (1, 2);
-
---
--- series
---
-
-CREATE TABLE `series`(
-  `series_id` int(11) NOT NULL PRIMARY KEY,
-  `title` VARCHAR(255) NOT NULL,
-  `age_range` enum('6', '9', '12', '16+', '18+')
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `series`(
-  `series_id`, `title`, `age_range`
-) VALUES (1, 'The exorcism', '16+');
-
---
--- episode
---
-
-CREATE TABLE `episode` (
-  `episode_id` int(11) NOT NULL PRIMARY KEY,
-  `title` VARCHAR(255),
-  `duration` TIME DEFAULT '00:00:00',
-  `series_id` int(11) NOT NULL,
-  FOREIGN KEY (`series_id`) REFERENCES `series`(`series_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `episode` (
-  `episode_id`, `title`, `duration`, `series_id`
-) VALUES (1, 'The exorcism', '00:20:12', 2);
-
---
--- series view count
---
-
-CREATE TABLE `seriesViewCount` (
-  `account_id` int(11) NOT NULL,
-  `series_id` int(11) NOT NULL,
-  `number` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`account_id`, `series_id`),
-  FOREIGN KEY (`account_id`) REFERENCES `user`(`account_id`),
-  FOREIGN KEY (`series_id`) REFERENCES `series`(`series_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `seriesViewCount` (
-  `account_id`, `series_id`, `number`
-) VALUES (1, 2, 1);
-
---
--- subtitle for movies
---
-
-CREATE TABLE `subtitleForMovies` (
-  `language_id` int(11) NOT NULL, 
-  `movie_id` int(11) NOT NULL,
-  PRIMARY KEY (`language_id`, `movie_id`),
-  FOREIGN KEY (`language_id`) REFERENCES `language`(`language_id`),
-  FOREIGN KEY (`movie_id`) REFERENCES `movie`(`movie_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `subtitleForMovies` (
-  `language_id`, `movie_id`
-) VALUES (1, 2);
-
---
---
---
-
---
--- subsitle for series 
---
-
-CREATE TABLE `subtitleForSeries` (
-  `language_id` int(11) NOT NULL, 
-  `series_id` int(11) NOT NULL,
-  PRIMARY KEY (`language_id`, `series_id`),
-  FOREIGN KEY (`language_id`) REFERENCES `language`(`language_id`),
-  FOREIGN KEY (`series_id`) REFERENCES `series`(`series_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `subtitleForSeries` (
-  `language_id`, `series_id`
-) VALUES (1, 2);
-
---
--- AUTO_INCREMENT for saved tables
---
-
---
--- AUTO_INCREMENT `language`
+-- Indexes for table `language`
 --
 ALTER TABLE `language`
-  MODIFY `language_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  ADD PRIMARY KEY (`language_id`);
 
 --
--- AUTO_INCREMENT `movie`
+-- Indexes for table `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  ADD PRIMARY KEY (`movie_id`);
 
 --
--- AUTO_INCREMENT `user`
+-- Indexes for table `moviesprofilewatchlist`
 --
-ALTER TABLE `user`
-  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `moviesprofilewatchlist`
+  ADD PRIMARY KEY (`profile_id`,`movie_id`);
 
 --
--- Foreign key constraints on stored tables
---
-
---
--- Table Foreign Key Constraints `movieviewcount`
+-- Indexes for table `movieviewcount`
 --
 ALTER TABLE `movieviewcount`
-  ADD CONSTRAINT `movieviewcount_movie` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`movie_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `movieviewcount_user` FOREIGN KEY (`account_id`) REFERENCES `user` (`account_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`account_id`,`movie_id`);
 
 --
--- Table Foreign Key Constraints `user`
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profile_id`);
+
+--
+-- Indexes for table `series`
+--
+ALTER TABLE `series`
+  ADD PRIMARY KEY (`series_id`);
+
+--
+-- Indexes for table `seriesprofilewatchlist`
+--
+ALTER TABLE `seriesprofilewatchlist`
+  ADD PRIMARY KEY (`account_id`,`series_id`);
+
+--
+-- Indexes for table `seriesviewcount`
+--
+ALTER TABLE `seriesviewcount`
+  ADD PRIMARY KEY (`account_id`,`series_id`);
+
+--
+-- Indexes for table `subtitleformovie`
+--
+ALTER TABLE `subtitleformovie`
+  ADD PRIMARY KEY (`language_id`,`movie_id`);
+
+--
+-- Indexes for table `subtitleforseries`
+--
+ALTER TABLE `subtitleforseries`
+  ADD PRIMARY KEY (`language_id`,`series_id`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `language_of_user` FOREIGN KEY (`language_id`) REFERENCES `language` (`language_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`account_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `episode`
+--
+ALTER TABLE `episode`
+  MODIFY `episode_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `language`
+--
+ALTER TABLE `language`
+  MODIFY `language_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `movie`
+--
+ALTER TABLE `movie`
+  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `series`
+--
+ALTER TABLE `series`
+  MODIFY `series_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `account_id` int(11) NOT NULL AUTO_INCREMENT;
