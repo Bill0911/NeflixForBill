6d3fecb0734a6bd1d7986599ec1aeb1fba163910
Bill0911 <do.do@student.nhlstenden.com>
2024-12-05 13:44:48 +0100

    Series part done

--- netflix/src/main/java/com/example/netflix/repository/SeriesViewCountRepository.java
+++ netflix/src/main/java/com/example/netflix/repository/SeriesViewCountRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SeriesViewCountRepository extends JpaRepository<MovieViewCount, Integer> {
-    Optional<SeriesViewCount> findByUser_AccountID_And_Series_id(Integer accountId, Integer movieId);
+public interface SeriesViewCountRepository extends JpaRepository<SeriesViewCount, Integer> {
+    Optional<SeriesViewCount> findByUser_AccountID_And_SeriesID(Integer accountId, Integer SeriesId);
 }
