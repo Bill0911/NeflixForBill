2e38ca252f6567f625dcd9cb27164da471dd0a30
Bill0911 <do.do@student.nhlstenden.com>
2024-12-09 16:01:42 +0100

    Add user interface files for login and register

--- netflix/src/main/java/com/example/netflix/frontEndFriendlyService/App.java
+++ netflix/src/main/java/com/example/netflix/frontEndFriendlyService/App.java
@@ -1,12 +1,27 @@
 package com.example.netflix.frontEndFriendlyService;
 
 import com.example.netflix.NetflixApplication;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import org.hibernate.id.factory.spi.StandardGenerator;
-
-//import javax.scene.Scene;
-//import javafx.scene.layout.StackPane;
-//import javafx.stage.Stage;
-public class App
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.application.Application;
+public class App extends Application
 {
+    @Override
+    public void start(Stage primaryStage)
+    {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 800, 600);
 
+        primaryStage.setTitle("User management");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 }
