342353bec73c25edae1e437ff127eab3f5ccfbfd
FjodorSmorodins <146731564+FjodorSmorodins@users.noreply.github.com>
2024-12-20 12:23:46 +0200

    xml and json

--- netflix/src/main/java/com/example/netflix/controller/PersonalizedOfferController.java
+++ netflix/src/main/java/com/example/netflix/controller/PersonalizedOfferController.java
@@ -9,6 +9,7 @@ import com.example.netflix.service.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 @RestController
