d927db1e1b2a8d85ba9a9366045288a96b637e79
FjodorSmorodins <146731564+FjodorSmorodins@users.noreply.github.com>
2024-12-25 15:01:54 +0200

    too complicated for now, not gonna try restricting like that

--- netflix/src/main/java/com/example/netflix/NetflixApplication.java
+++ netflix/src/main/java/com/example/netflix/NetflixApplication.java
@@ -2,8 +2,10 @@ package com.example.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.netflix.security", "com.example.netflix.config"})
 public class NetflixApplication {
 
 	public static void main(String[] args) {
