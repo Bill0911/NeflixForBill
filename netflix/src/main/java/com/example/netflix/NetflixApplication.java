c81b0bd835d2d37d8f668459d0468acd581b2fb5
Bill0911 <do.do@student.nhlstenden.com>
2024-12-26 21:14:12 +0100

    Fixed security issue

--- netflix/src/main/java/com/example/netflix/NetflixApplication.java
+++ netflix/src/main/java/com/example/netflix/NetflixApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.netflix.security", "com.example.netflix.config"})
 public class NetflixApplication {
 
 	public static void main(String[] args) {
