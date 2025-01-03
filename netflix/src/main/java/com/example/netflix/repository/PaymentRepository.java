3dae3dba16fe1e60bebeaf0f038bd46eb76b715e
Bill0911 <do.do@student.nhlstenden.com>
2025-01-03 19:14:52 +0100

    tested Payment APIs passed

--- netflix/src/main/java/com/example/netflix/repository/PaymentRepository.java
+++ netflix/src/main/java/com/example/netflix/repository/PaymentRepository.java
@@ -1,10 +1,19 @@
 package com.example.netflix.repository;
 
 import com.example.netflix.entity.Payment;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer>
 {
+    @Modifying
+    @Transactional
+    @Query("UPDATE Payment p SET p.nextBillingDate = :nextBillingDate WHERE p.user.accountId = :userId")
+    void updateNextBillingDate(Integer userId, LocalDateTime nextBillingDate);
 }
