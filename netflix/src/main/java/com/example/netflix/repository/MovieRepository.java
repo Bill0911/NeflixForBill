939939de4b1b13aac1f2b73059875ef75c3430b5
Fyodor Smorodin <fjodsmorod@gmail.com>
2025-01-06 02:06:45 +0100

    I wanna leave seriescontroller iwth errors for now :}

--- netflix/src/main/java/com/example/netflix/repository/MovieRepository.java
+++ netflix/src/main/java/com/example/netflix/repository/MovieRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Integer> {\
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
     @Modifying
     @Transactional
