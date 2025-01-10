e193dd1b568fd4e669c69ba2c4e1a9a3a5629ea0
Bill0911 <do.do@student.nhlstenden.com>
2025-01-10 19:44:37 +0100

    add priveleges on junior and medior and edit senior a bit on senior. SHould add '@' and '%' beside user names as we willl have issues with connecting host if we remove it

--- users_restrictions.sql
+++ users_restrictions.sql
@@ -1,3 +1,17 @@
 CREATE ROLE senior;
-GRANT SELECT, UPDATE, INSERT, DELETE on netflix.* to senior;
-CREATE user 'fjodorsenior' identified by '1234' default ROLE 'senior';
+GRANT SELECT, UPDATE, INSERT, DELETE ON netflix.* TO senior;
+CREATE USER 'fjodorsenior'@'%' IDENTIFIED BY '1234' DEFAULT ROLE senior;
+GRANT senior TO 'fjodorsenior'@'%';
+
+CREATE ROLE medior;
+GRANT SELECT, UPDATE, INSERT, DELETE ON netflix.* TO medior;
+REVOKE SELECT, UPDATE, INSERT, DELETE ON netflix.payments FROM medior;
+CREATE USER 'zhimedior'@'%' IDENTIFIED BY '5678' DEFAULT ROLE medior;
+GRANT medior TO 'zhimedior'@'%';
+
+CREATE ROLE junior;
+GRANT SELECT, UPDATE, INSERT, DELETE ON netflix.* TO junior;
+REVOKE SELECT, UPDATE, INSERT, DELETE ON netflix.payments FROM junior;
+REVOKE SELECT (email, payment_method) ON netflix.user FROM junior;
+CREATE USER 'billjunior'@'%' IDENTIFIED BY '8765' DEFAULT ROLE junior;
+GRANT junior TO 'billjunior'@'%';
