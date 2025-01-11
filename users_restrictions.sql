3fdce35d2697b496bdac4e89084d34757cf8d988
Fyodor Smorodin <fjodsmorod@gmail.com>
2025-01-11 20:19:04 +0100

    api user should be according to the assignment

--- users_restrictions.sql
+++ users_restrictions.sql
@@ -1,7 +1,7 @@
 DROP ROLE IF EXISTS senior;
 DROP ROLE IF EXISTS medior;
 DROP ROLE IF EXISTS junior;
-DROP ROLE IF EXISTS api_user;
+DROP ROLE IF EXISTS api;
 
 DROP USER IF EXISTS 'fjodorsenior'@'%';
 DROP USER IF EXISTS 'zhimedior'@'%';
@@ -11,7 +11,7 @@ DROP USER IF EXISTS 'ghost_api'@'%';
 CREATE ROLE senior;
 CREATE ROLE medior;
 CREATE ROLE junior;
-CREATE ROLE api_user;
+CREATE ROLE api;
 
 -- ---------------SENNIOR PERMISSIONS-----------------------
 GRANT SELECT, UPDATE, INSERT, DELETE ON netflix.* TO senior;
@@ -54,6 +54,20 @@ GRANT SELECT, UPDATE, INSERT, DELETE ON netflix.seriesviewcount TO junior;
 GRANT SELECT ON netflix.user_for_junior TO junior;
 -- ---------------------------------------------------------
 
+-- --------------------------API-------------------------------
+
+GRANT EXECUTE ON netflix.* TO api;
+
+SET @role = 'api';
+SELECT CONCAT('GRANT SELECT ON `netflix`.`', TABLE_NAME, '` TO ''', @role, ''';')
+FROM information_schema.tables
+WHERE TABLE_SCHEMA = @db AND TABLE_TYPE = 'VIEW';
+
+-- ---------------------------------------------------------
+
+
+    
+-- ---------------------------------------------------------
 
 CREATE USER 'fjodorsenior'@'%' IDENTIFIED BY '1234';
 GRANT senior TO 'fjodorsenior'@'%';
@@ -64,10 +78,13 @@ GRANT medior TO 'zhimedior'@'%';
 CREATE USER 'billjunior'@'%' IDENTIFIED BY '8765';
 GRANT junior TO 'billjunior'@'%';
 
-CREATE USER 'ghost_api'@'%' IDENTIFIED BY 'ghost';
+CREATE USER 'main_api_user'@'%' IDENTIFIED BY '1234api';
+GRANT 'api' TO 'main_api_user'@'%';
+SET DEFAULT ROLE 'api' FOR 'main_api_user'@'%';
 
-GRANT EXECUTE ON PROCEDURE netflix.getManyUsers TO ghost_api;
-GRANT SELECT ON netflix.user_view TO ghost_api;
+
+
+
 
 
 
