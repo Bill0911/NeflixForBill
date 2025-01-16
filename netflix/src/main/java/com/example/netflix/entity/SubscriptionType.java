30e59ede877e941ef463bbe6fd3ccae7617c71f5
Fyodor Smorodin <fjodsmorod@gmail.com>
2025-01-16 16:47:17 +0100

    value scaling for subscription

--- netflix/src/main/java/com/example/netflix/entity/SubscriptionType.java
+++ netflix/src/main/java/com/example/netflix/entity/SubscriptionType.java
@@ -2,5 +2,24 @@ package com.example.netflix.entity;
 
 public enum SubscriptionType
 {
-    SD, HD, UHD
+    SD(1),
+    HD(2),
+    UHD(3);
+    private final Integer value;
+
+    SubscriptionType(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public boolean isHigherThan(Role other) {
+        return this.value > other.getValue();
+    }
+
+    public boolean isLowerThan(Role other) {
+        return this.value < other.getValue();
+    }
 }
