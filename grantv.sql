35afeed218e1a3a6dbc846403f92e777911ad001
Fyodor Smorodin <fjodsmorod@gmail.com>
2025-03-03 16:31:18 +0100

    transaction in separate file

--- grantv.sql
+++
@@ -1,3 +0,0 @@
-GRANT SELECT ON `netflix`.`paymentstatus` TO api;
-GRANT SELECT ON `netflix`.`subscription_cost` TO api;
-GRANT SELECT ON `netflix`.`user_genre_count` TO api;
--- netflix.sql
+++ netflix.sql
@@ -1603,20 +1603,3 @@ DELIMITER $$
 CREATE EVENT `InsertExpiredTrialsDaily` ON SCHEDULE EVERY 1 DAY STARTS '2025-01-04 19:44:33' ON COMPLETION NOT PRESERVE ENABLE DO CALL `InsertExpiredTrialsIntoPayments`()$$
 
 DELIMITER ;
-
-
-START TRANSACTION
-
-INSERT INTO `user` (`account_id`, `email`, `password`, `payment_method`, `active`, `blocked`, `subscription`, `trial_start_date`, `trial_end_date`, `language_id`, `role`, `failed_attempts`, `lock_time`, `discount`) VALUES 
-(25, 'janjassen@gmail.com', '$2a$10$7v5Ho908rYS1FPGp.N.7nOjJuJcRxnkSkaFPNFtXVPwHJhlREVW12', 'IDEAL', b'1', b'0', 'HD', '2025-01-15 17:30:45', NULL, 1, 'VIEWER', 0, NULL, b'1'),
-(26, 'billJ1@outlook.com', '$2a$10$cvns7c5I6KUpgymcO610D.6CBWdZJAl90undCy/AZzLsIGSFkvJXi', 'IDEAL', b'1', b'0', 'HD', '2025-01-17 13:56:32', NULL, 1, 'VIEWER', 0, NULL, b'1');
-
-UPDATE `user` SET `payment_method` = 'newly created card' WHERE `account_id` = 3;
-
-SAVEPOINT savepoint1;
-
-DELETE FROM `genre` WHERE `genre_id` = 29;
-
-ROLLBACK TO savepoint1;
-
-COMMIT;
